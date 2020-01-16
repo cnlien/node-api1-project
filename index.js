8648a9dbb4920b528351e2b45fe9f16c04a393b0
Christopher Lien <cnlien@gmail.com>
2020-01-16 10:08:59 -0500

    Get users is working

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const server = express();
 
 server.listen(4000, () => {
     console.log('===server listening on port 4000===')
-})
+});
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Node API Project 1');
+})
+
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({success: false, err});
+        })
+})
