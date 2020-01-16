37f60a768610beb08770facc64af3d3588d52da0
Christopher Lien <cnlien@gmail.com>
2020-01-16 09:52:28 -0500

    sever is running on port 400

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const db = require('./data/db.js');
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('===server listening on port 4000===')
+})
+
+server.use(express.json());
